133379d22db8ca034383be0684c2976e825b6d67
Jon Hunter <jhunter@technome.io>
2024-07-11 13:44:48 -0400

    Initial Commit - Scaffolding

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject } from "@angular/core";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { NavigationEnd, Router, RouterLink, RouterOutlet } from "@angular/router";
+import { filter, map, startWith } from "rxjs";
+import { ArrowBackIconComponent } from "./components/icons/arrow-back-icon.component";
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  imports: [RouterOutlet, ArrowBackIconComponent, RouterLink],
+  templateUrl: "./app.component.html",
+  styleUrl: "./app.component.scss",
+})
+export class AppComponent {
+  private readonly router = inject(Router);
+  private readonly isMainPage$ = this.router.events.pipe(
+    filter((event): event is NavigationEnd => event instanceof NavigationEnd),
+    map((event: NavigationEnd) => event.url === "/"),
+    startWith(true)
+  );
+
+  isMainPage = toSignal(this.isMainPage$);
+}
