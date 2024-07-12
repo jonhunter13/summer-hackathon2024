60c6b4d3e4e7f7683af4e7cc7bd3771f47c6e4cb
Kevin Soto <kevin2005_10@hotmail.com>
2024-07-12 02:35:06 -0500

    Update 1.3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from "@angular/core";
 import { RouterLink, RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { SharedModule } from './shared.module'; // Ajusta la ruta según tu estructura de archivos
 
 @Component({
   selector: "app-root",
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterOutlet, RouterLink],
+  imports: [HeaderComponent, FooterComponent, RouterOutlet, RouterLink, SharedModule],
   templateUrl: "./app.component.html",
   styleUrl: "./app.component.scss",
 })
---
+++ src/app/components/speaker/speaker.component.ts
@@ -0,0 +1,27 @@
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+@Component({
+  selector: 'app-speaker',
+  templateUrl: './speaker.component.html',
+  styleUrls: ['./speaker.component.scss']
+})
+export class SpeakerComponent {
+  
+  @ViewChild('audioPlayer', { static: false }) audioPlayerRef!: ElementRef<HTMLAudioElement>; // Referencia al elemento audio
+  isPlaying: boolean = false;
+
+  constructor() {}
+
+  toggleSound(): void {
+    const audioPlayer = this.audioPlayerRef.nativeElement;
+
+    if (this.isPlaying) {
+      audioPlayer.pause(); // Pausar reproducción
+    } else {
+      audioPlayer.volume = 0.6; // Establecer volumen al 60%
+      audioPlayer.play(); // Iniciar reproducción
+    }
+    
+    this.isPlaying = !this.isPlaying;
+  }
+}
