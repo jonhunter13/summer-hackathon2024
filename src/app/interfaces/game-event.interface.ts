8c05260967073c3f1ab2cd006e57205fcaf2778f
Jon Hunter <jonhunter13@gmail.com>
2024-07-12 06:16:34 -0400

    Basic events page

---
+++ src/app/interfaces/game-event.interface.ts
@@ -0,0 +1,22 @@
+export interface GameEvent {
+  title: string;
+  description: string;
+  date: string;
+  time: string;
+  duration: string;
+}
+
+export const ALL_MONTHS = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
--- src/app/pages/events/events.component.ts
+++ src/app/pages/events/events.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import events from "../../../assets/datasources/events.json";
+import { PageHeaderEventsComponent } from "../../components/icons/page-header-events.component";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { NgFor } from "@angular/common";
+import { ALL_MONTHS, GameEvent } from "../../interfaces/game-event.interface";
 
 @Component({
-  selector: 'app-events',
+  selector: "app-events",
   standalone: true,
-  imports: [],
-  templateUrl: './events.component.html',
-  styleUrl: './events.component.scss'
+  imports: [MatGridListModule, NgFor, PageHeaderEventsComponent],
+  templateUrl: "./events.component.html",
+  styleUrl: "./events.component.scss",
 })
-export class EventsComponent {
+export class EventsComponent implements OnInit {
+  events = events as GameEvent[];
+  month: string | undefined;
+  currentMonth = new Date().getMonth();
+  constructor() {}
 
+  ngOnInit() {
+    this.month = ALL_MONTHS[this.currentMonth];
+  }
+
+  getNextMonth() {
+    this.currentMonth = (this.currentMonth + 1) % 12;
+  }
+
+  getPreviousMonth() {
+    this.currentMonth = (this.currentMonth - 1 + 12) % 12;
+  }
 }
